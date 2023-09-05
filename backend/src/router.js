1308844b89b1070d28ba44708d3e23e1ec08c034
Jack <jackvwh@hotmail.com>
2023-09-05 22:55:43 +0200

    added main router

---
+++ backend/src/router.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import artistsRouter from "./routes/artists.router.js"; 
+import homeRouter from "./routes/home.router.js";
+
+export default Router()
+    .use('/', homeRouter)
+    .use('/artists', artistsRouter);
