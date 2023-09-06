4738150b71327519d3511431cbfd90751360f13d
Jack <jackvwh@hotmail.com>
2023-09-06 21:29:06 +0200

    refactor file structure

--- frontend/app/JS/helpers/showAllArtists.js
+++
@@ -1,8 +0,0 @@
-import artistCard from "../components/artistCard.js";
-
-export default function showAllArtists(list) {
-    document.querySelector("#artist-grid").innerHTML = "";
-    for (const artist of list) {
-        artistCard(artist);
-    }
-}
