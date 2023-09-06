53ff0e597d5710611bdf299896f1324b2aaa437e
Jack <jackvwh@hotmail.com>
2023-09-06 21:30:01 +0200

    rf

--- frontend/app/JS/helpers/refreshArtistsList.js
+++
@@ -1,8 +0,0 @@
-import showAllArtists from "./showAllArtists.js";
-import { readArtists } from "../services/artists.services.js";
-
-export default async function refreshArtistsList() {
-    console.log('refreshArtistsList');
-    const artists = await readArtists();
-    showAllArtists(artists);
-}
