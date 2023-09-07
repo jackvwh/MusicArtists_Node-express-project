d930771669f45beadfb892f6c2dae693d863b68b
Jack <jackvwh@hotmail.com>
2023-09-07 10:55:24 +0200

    favorite star added

--- frontend/app/JS/components/cards/artistCard.js
+++ frontend/app/JS/components/cards/artistCard.js
@@ -1,12 +1,31 @@
 import { deleteArtist, favoriteArtist } from "../../services/artists.services.js";
 import { selectArtist } from "../../helpers/artists.helpers.js";
 
+function createStarSVG() {
+    return /*html*/ `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
+      </svg>
+    `;
+  }
+
 export default function artistCard(artist){
 
     document.querySelector("#artist-grid").insertAdjacentHTML(
         "beforeend", /*html*/ `
         <article id="${artist.id}">
             <img src="${artist.image}">
+            <h4>${artist.favorite ? createStarSVG() : ""}</h4>
             <h2>Name: ${artist.name}</h2>
             <h3>Active since: ${artist.activeSince}</h3>
             <h3>Birthday: ${artist.birthdate}</h3>
